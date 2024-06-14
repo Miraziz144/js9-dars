f7924aef43e923795f0b029179e9f320b50ce9f2
Miraziz144 <nurullayevmiraziz4@gmail.com>
2024-06-14 12:24:21 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let nums = [2,4.6,7,8,9,10,11]
+
+let mavalar = ['olma', 'anor','banan', 'nok','gilos','olma','uzum','apelsin','shaftoli','qulupnay'];
+
+
+for (const child of mavalar){
+    if (child.includes('a')){
+        console.log(child + 'mevalar a harfi bor');
+    }
+}
+
